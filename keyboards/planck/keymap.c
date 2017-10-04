36f7c678f7176441c21c7d44cc8d8c6be26c7382
Taro Hanamura <m@hanamurataro.com>
2017-10-04 20:32:42 +0900

    Update keymap

--- keyboards/planck/keymap.c
+++ keyboards/planck/keymap.c
@@ -26,27 +26,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC},
   {MT(MOD_LCTL,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT},
   {KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT},
-  {BACKLIT, KC_LCTL, KC_LALT, MT(MOD_LGUI,KC_LANG2), LOWER, KC_SPC, KC_SPC, RAISE, MT(KC_RGUI,KC_LANG1), KC_RALT, _______, _______}
+  {BACKLIT, KC_LCTL, KC_LALT, MT(MOD_LGUI,KC_LANG2), LOWER, KC_SPC, KC_SPC, RAISE, MT(KC_RGUI,KC_LANG1), KC_RALT, KC_RCTL, _______}
 },
 
 [_LOWER] = {
-  {KC_GRAVE, _______, _______, KC_UP, _______, KC_LPRN, KC_RPRN, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_SLASH, KC_BSPC},
-  {_______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, KC_LBRC, KC_RBRC, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ASTERISK, KC_KP_EQUAL},
-  {_______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_MINUS, _______},
-  {_______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_KP_0, KC_KP_COMMA, KC_KP_DOT, KC_KP_PLUS, KC_KP_ENTER}
+  {KC_ESC, _______, KC_AMPR, KC_ASTR, _______, KC_SLASH, KC_BSLS, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______},
+  {_______, _______, KC_DLR, KC_PERC, KC_CIRC, KC_MINS, KC_EQL, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______},
+  {_______, _______, KC_EXLM, KC_AT, KC_HASH, KC_GRAVE, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_COMMA, KC_KP_DOT, _______, _______}
 },
 
 [_RAISE] = {
-  {KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
-  {LGUI(LSFT(LCTL(KC_4))), _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
+  {KC_ESC, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, KC_LT, KC_GT, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 [_ADJUST] = {
-  {_______, RESET,   DEBUG,   _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______, _______, _______},
-  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
+  {LGUI(LSFT(LCTL(KC_4))), _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, LGUI(LSFT(KC_LBRC)), LGUI(LSFT(KC_RBRC)), _______, _______, _______},
+  {_______, _______, _______, _______, KC_WH_L, KC_WH_U, KC_WH_R, LGUI(KC_LBRC), LGUI(KC_RBRC), _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 }
 
