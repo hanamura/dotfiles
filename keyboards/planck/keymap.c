0c66333ee93370187789bf8d9d3014fd92dc75e3
Taro Hanamura <m@hanamurataro.com>
2017-10-04 21:33:31 +0900

    Update keymap

--- keyboards/planck/keymap.c
+++ keyboards/planck/keymap.c
@@ -3,6 +3,14 @@
 
 extern keymap_config_t keymap_config;
 
+#define CAPTURE LGUI(LSFT(LCTL(KC_4)))
+#define PREV_TAB LGUI(LSFT(KC_LBRC))
+#define NEXT_TAB LGUI(LSFT(KC_RBRC))
+#define PREV_WIN LGUI(KC_LBRC)
+#define NEXT_WIN LGUI(KC_RBRC)
+#define CMD_EISU MT(MOD_LGUI,KC_LANG2)
+#define CMD_KANA MT(MOD_RGUI,KC_LANG1)
+
 enum planck_layers {
   _QWERTY,
   _LOWER,
@@ -23,31 +31,31 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = {
-  {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC},
-  {MT(MOD_LCTL,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT},
-  {KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT},
-  {BACKLIT, KC_LCTL, KC_LALT, MT(MOD_LGUI,KC_LANG2), LOWER, KC_SPC, KC_SPC, RAISE, MT(KC_RGUI,KC_LANG1), KC_RALT, KC_RCTL, _______}
+  {KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,     KC_O,    KC_P,    KC_BSPC},
+  {KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,  KC_G,   KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_ENT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH, KC_QUOT},
+  {BACKLIT, KC_LCTL, KC_LALT, CMD_EISU, LOWER, KC_SPC, KC_SPC, RAISE, CMD_KANA, KC_RALT, KC_RCTL, _______}
 },
 
 [_LOWER] = {
-  {KC_ESC, _______, KC_AMPR, KC_ASTR, _______, KC_SLASH, KC_BSLS, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______},
-  {_______, _______, KC_DLR, KC_PERC, KC_CIRC, KC_MINS, KC_EQL, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______},
-  {_______, _______, KC_EXLM, KC_AT, KC_HASH, KC_GRAVE, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_COMMA, KC_KP_DOT, _______, _______}
+  {KC_ESC,  _______, KC_AMPR, KC_ASTR, _______, KC_SLASH, KC_BSLS, KC_KP_7, KC_KP_8,  KC_KP_9,   _______, _______},
+  {_______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_MINS,  KC_EQL,  KC_KP_4, KC_KP_5,  KC_KP_6,   _______, _______},
+  {_______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_GRAVE, KC_KP_0, KC_KP_1, KC_KP_2,  KC_KP_3,   _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______,  KC_COMMA, KC_KP_DOT, _______, _______}
 },
 
 [_RAISE] = {
-  {KC_ESC, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______},
+  {KC_ESC,  _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, KC_LT, KC_GT, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, KC_LT,   KC_GT,   _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 [_ADJUST] = {
-  {LGUI(LSFT(LCTL(KC_4))), _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, LGUI(LSFT(KC_LBRC)), LGUI(LSFT(KC_RBRC)), _______, _______, _______},
-  {_______, _______, _______, _______, KC_WH_L, KC_WH_U, KC_WH_R, LGUI(KC_LBRC), LGUI(KC_RBRC), _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {KC_ESC,  _______, KC_BTN1, KC_MS_U, KC_BTN2, _______,  _______,  _______, KC_WH_D, _______, _______, _______},
+  {_______, _______, KC_MS_L, KC_MS_D, KC_MS_R, PREV_TAB, NEXT_TAB, KC_WH_R, KC_WH_U, KC_WH_L, _______, _______},
+  {_______, _______, _______, _______, _______, PREV_WIN, NEXT_WIN, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, CAPTURE,  _______,  _______, _______, _______, _______, _______}
 }
 
 };
