46d0d12fc34b00b89a91a4c7bcaca1ff9ae314c2
Taro Hanamura <m@hanamurataro.com>
2017-12-14 17:10:25 +0900

    New keymap

--- keyboards/lets_split/hanamura/keymap.c
+++ keyboards/lets_split/hanamura/keymap.c
@@ -9,12 +9,12 @@
 #define NEXT_PANE LGUI(KC_RBRC)
 #define CMD_EISU MT(MOD_LGUI,KC_LANG2)
 #define CMD_KANA MT(MOD_RGUI,KC_LANG1)
-#define NEXT_WORD LALT(KC_RIGHT)
-#define PREV_WORD LALT(KC_LEFT)
 #define NEXT_WIN LGUI(KC_GRV)
 #define ELLIPSIS LALT(KC_SCLN)
 #define SANDS SFT_T(KC_SPC)
 #define CTL_ESC MT(MOD_LCTL,KC_ESC)
+#define TRU_DQT LALT(KC_LBRC)
+#define TRU_QT LALT(KC_RBRC)
 
 extern keymap_config_t keymap_config;
 
@@ -36,28 +36,28 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = KEYMAP( \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,     KC_O,    KC_P,    KC_BSPC, \
-  CTL_ESC, KC_A,    KC_S,    KC_D,     KC_F,  KC_G,  KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,  KC_B,  KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH, KC_QUOT , \
-  RAISE,   KC_LCTL, KC_LALT, CMD_EISU, LOWER, SANDS, KC_SPC, RAISE, CMD_KANA, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,  KC_T,    KC_Y,   KC_U,  KC_I,     KC_O,    KC_P,    KC_BSPC, \
+  CTL_ESC, KC_A,    KC_S,    KC_D,     KC_F,  KC_G,    KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_ENT,  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,  KC_B,    KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, \
+  RAISE,   KC_LCTL, KC_LALT, CMD_EISU, LOWER, KC_LSFT, KC_SPC, LOWER, CMD_KANA, XXXXXXX, XXXXXXX, RAISE    \
 ),
 
 [_LOWER] = KEYMAP( \
-  KC_GRV,  _______, _______, _______, ELLIPSIS, KC_SLSH, KC_BSLS, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, \
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-  _______, _______, _______, KC_DQT,  KC_UNDS,  KC_GRV,  KC_QUOT, KC_MINS, KC_PLUS, KC_EQL,  _______, _______, \
-  _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______ \
+  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS, \
+  _______, KC_TILD, _______, KC_DQT,  KC_QUOT, KC_UNDS, KC_MINS, KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______, \
+  _______, _______, _______, TRU_DQT, TRU_QT,  KC_PLUS, KC_EQL,  ELLIPSIS, KC_PIPE, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______  \
 ),
 
 [_RAISE] = KEYMAP( \
-  KC_GRV,  _______, _______, KC_UP,   _______,  KC_F5,   _______, KC_P7,   KC_P8,   KC_P9,   _______, _______, \
-  _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_PDOT, KC_P4,   KC_P5,   KC_P6,   _______, _______, \
-  _______, _______, _______, _______, _______,  _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   _______, _______, \
-  _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______ \
+  _______, _______, _______, KC_UP,   _______, _______, _______, KC_P7,   KC_P8, KC_P9,   KC_PSLS, _______, \
+  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PMNS, KC_P4,   KC_P5, KC_P6,   KC_PAST, _______, \
+  _______, _______, _______, _______, _______, _______, KC_PEQL, KC_P1,   KC_P2, KC_P3,   KC_PPLS, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_P0, KC_PDOT, KC_PCMM, _______ \
 ),
 
 [_ADJUST] = KEYMAP( \
-  KC_GRV,  _______, _______, KC_WH_D,     _______,   _______,   _______,   KC_BTN1, KC_MS_U,  KC_BTN2, RGB_HUI, RGB_HUD, \
+  _______, _______, _______, KC_WH_D,     _______,   _______,   _______,   KC_BTN1, KC_MS_U,  KC_BTN2, RGB_HUI, RGB_HUD, \
   _______, _______, KC_WH_R, KC_WH_U,     KC_WH_L,   PREV_TAB,  NEXT_TAB,  KC_MS_L, KC_MS_D,  KC_MS_R, RGB_SAI, RGB_SAD, \
   _______, RESET,   _______, KC__VOLDOWN, KC__VOLUP, PREV_PANE, NEXT_PANE, RGB_M_P, RGB_M_SW, RGB_M_X, RGB_VAI, RGB_VAD, \
   _______, _______, _______, _______,     _______,   CAPTURE,   NEXT_WIN,  _______, _______,  _______, _______, RGB_TOG \
