0bb4f3e79194b45e3ffa3ef8c805d85a05897058
Taro Hanamura <m@hanamurataro.com>
2018-03-10 00:47:18 +0900

    Drop QMK Firmware and keymaps
    See https://github.com/hanamura/qmk_firmware instead

--- keyboards/lets_split/hanamura/rules.mk
+++
@@ -1,6 +0,0 @@
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
-
-RGBLIGHT_ENABLE = yes
-TAP_DANCE_ENABLE = yes
--- keyboards/planck/hanamura/rules.mk
+++
@@ -1,8 +0,0 @@
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
-
-AUDIO_ENABLE = no
-MIDI_ENABLE = no
-MOUSEKEY_ENABLE = yes
-TAP_DANCE_ENABLE = yes
